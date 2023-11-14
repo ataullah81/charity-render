6f2279cac5e87ad17f7733a7cd43982e5c7bf9d1
Ataullah Behesti <ataullah.behesti@gmail.com>
2023-11-14 15:36:06 +0200

    index page url changed

--- charityfinderapp/urls.py
+++ charityfinderapp/urls.py
@@ -5,7 +5,7 @@ from charityfinderapp import views
 
 urlpatterns = [
     path('charity', views.charity_func, name='charity'),
-    path('index', views.index_func, name='index'),
+    path('', views.index_func, name='index'),
     path('about', views.about_func, name='about'),
     path('donate', views.donate_func, name='donate'),
     path('search_charity', views.search_charity_func, name='search_charity'),
